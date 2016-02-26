ef7359a41beff165c6b0197e25c684ee17512938
gousseinm <perrym@mail.ru>
2016-02-27 01:23:43 +0300

    minor refactoring

--- Test/src/Main.kt
+++ Test/src/Main.kt
@@ -4,9 +4,6 @@ fun main(args: Array<String>) {
     try {
         val params = ParamsConfig(args)
         params.print()
-
-        val a = intArrayOf(1, 2, 3, 4)
-        println(a.filter({x -> x % 2 == 0}))
     } catch (e: Exception) {
         println(e.message)
     }
